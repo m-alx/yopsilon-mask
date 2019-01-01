7477f924efcb7691770ffd12697d71329abf5c02
m-alx <m_alx@list.ru>
2019-01-01 19:23:07 +0300

    Export classes

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,6 @@
 export * from './src/yopsilon-mask.module';
+export { Mask } from './src/mask/mask.class';
+export { MaskOptions } from './src/mask/mask-options.class';
+export { Internationalization } from './src/internationalization/internationalization.class';
+export { Locale } from './src/internationalization/locale.class';
+export { LocaleRu } from './src/internationalization/locales/ru-ru';
