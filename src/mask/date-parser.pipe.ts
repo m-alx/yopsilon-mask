0a2c53de29b2a86e170de8923f51752f55e7397d
m-alx <m_alx@list.ru>
2019-01-08 16:12:46 +0300

    Date parser pipe

---
+++ src/mask/date-parser.pipe.ts
@@ -0,0 +1,128 @@
+// Copyright (C) 2018 Aleksey Melnikov
+// mailto: z9115011@gmail.com
+// This project is licensed under the terms of the MIT license.
+// https://github.com/m-alx/yopsilon-mask
+
+import { Pipe } from "@angular/core";
+
+import { Mask } from "./mask.class";
+import { MaskSection } from "./mask-section.class";
+import { MaskSectionType } from "./mask-section-type.class";
+
+// Парсинг даты по  заданной маске
+@Pipe({
+  name: "ynDateParser",
+  pure: true
+})
+export class DateParserPipe {
+
+    transform(mask: Mask, value: string): any {
+
+      let sectionPos = 0;
+      let res = value;
+
+      let d: number = 1;
+      let m: number = 1;
+      let y: number = 1900;
+
+      let hh: number = 0;
+      let mi: number = 0;
+      let ss: number = 0;
+      let ms: number = 0;
+
+      let tt: string = "";
+
+      for(let i = 0; i < mask.sections.length; i++) {
+
+        let section: MaskSection = mask.sections[i];
+        let datePart = section.sectionType.datePart;
+
+        if(datePart == null) // Не является частью даты
+          continue;
+
+        let v = section.extractSectionValue(res, sectionPos);
+        sectionPos = v.nextSectionPos();
+
+        // Значение секции
+        let s: string = v.sectionValue.value();
+
+        let n: number;
+        n = NaN;
+
+        if(section.isNumeric()) {
+          n = section.numericValue(s);
+          if(n < section.sectionType.min || n > section.sectionType.max)
+            return this.invalidDate();
+        }
+        else
+          if(section.hasVariants()) {
+            n = section.sectionType.variants.indexOf(s);
+            if(n < 0)
+              return this.invalidDate();
+            n++; // Индекс начинается с нуля
+          }
+
+        if(n == NaN)
+          return this.invalidDate();
+
+        // Время...
+        if(datePart == "H")
+          hh = n;
+
+        if(datePart == "h") {
+          hh = n;
+          if(hh == 12)
+            hh = 0;
+        }
+
+        if(datePart == "tt")
+          tt = s;
+
+        if(datePart == "mi")
+          mi = n;
+
+        if(datePart == "ss")
+          ss = n;
+
+        if(datePart == "ms")
+          ms = n;
+
+        // Дата...
+        if(datePart == "d")
+          d = n;
+
+        if(datePart == "m")
+          m = n;
+
+        if(datePart == "yy") {
+          if(n < 30)
+            y = 2000 + n;
+          else
+            y = 1900 + n;
+        }
+
+        if(datePart == "yyyy")
+          y = n;
+      }
+
+      if(tt.toLowerCase() == "pm")
+        hh += 12;
+
+      // Лишь одно мы проверим. Количество дней в месяце...
+      let maxDays: number = this.daysInMonth(y, m);
+      if(d > maxDays)
+        return this.invalidDate();
+
+      return new Date(y, m - 1, d, hh, mi, ss, ms);
+    }
+
+    // Создаем Invalid Date
+    // Может быть кто-нибудь научит, как сделать это правильно
+    invalidDate() {
+      return new Date('*');
+    }
+
+    daysInMonth (y: number, m: number): number {
+      return new Date(y, m, 0).getDate();
+    }
+}
---
+++ tests/date-parser.pipe.spec.ts
@@ -0,0 +1,56 @@
+// Copyright (C) 2018 Aleksey Melnikov
+// This project is licensed under the terms of the MIT license.
+// https://github.com/m-alx/yopsilon-mask
+
+import { async } from '@angular/core/testing';
+
+import { Internationalization } from "../src/internationalization/internationalization.class";
+import { MaskSection, MaskSectionKeyResult } from "../src/mask/mask-section.class";
+import { MaskValue } from "../src/mask/mask-value.class";
+import { MaskOptions } from "../src/mask/mask-options.class";
+import { Mask } from "../src/mask/mask.class";
+
+import { DateParserPipe } from "../src/mask/date-parser.pipe";
+
+describe(`Parse datetime by Mask (mask=MM/dd/yy h:mi tt, value=01/05/19 1:30 pm)`, () => {
+
+  let intl = new Internationalization();
+  let mask = new Mask(intl);
+  mask.mask = "MM/dd/yy h:mi tt";
+
+  let dateString = "01/05/19 1:30 pm";
+  let parser = new DateParserPipe();
+  let res = parser.transform(mask, dateString); // MaskDate.parseDate(mask.sections, dateString);
+
+  it(`Month = 0`, () => expect(res.getMonth()).toBe(0));
+  it(`Day = 5`, () => expect(res.getDate()).toBe(5));
+  it(`Year = 2019`, () => expect(res.getFullYear()).toBe(2019));
+  it(`Hours = 13`, () => expect(res.getHours()).toBe(13));
+  it(`Minutes = 30`, () => expect(res.getMinutes()).toBe(30));
+});
+
+describe(`Parse not matched date (mask=MM/dd/yy h:mi tt, value=01/05/19 1:30)`, () => {
+
+  let intl = new Internationalization();
+  let mask = new Mask(intl);
+  mask.mask = "MM/dd/yy h:mi tt";
+
+  let dateString = "01/05/19 1:30";
+  let parser = new DateParserPipe();
+  let res = parser.transform(mask, dateString); // MaskDate.parseDate(mask.sections, dateString);
+
+  it(`Result must be Invalid Date`, () => expect(res.getTime()).toBeNaN());
+});
+
+describe(`Parse invalid date (mask=MM/dd/yy, value=02/31/19)`, () => {
+
+  let intl = new Internationalization();
+  let mask = new Mask(intl);
+  mask.mask = "MM/dd/yy";
+
+  let dateString = "02/31/19";
+  let parser = new DateParserPipe();
+  let res = parser.transform(mask, dateString); // MaskDate.parseDate(mask.sections, dateString);
+
+  it(`Result must be Invalid Date`, () => expect(res.getTime()).toBeNaN()));
+});
