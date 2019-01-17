70baeb638ede74bb4fc36750d13a871ed29a593c
m-alx <m_alx@list.ru>
2019-01-17 17:05:47 +0300

    Misprint (zh_CN instead of zh-CN)

--- src/internationalization/locales/zh-cn.ts
+++ src/internationalization/locales/zh-cn.ts
@@ -12,7 +12,7 @@ export class LocaleChinese {
 
     let locale: Locale = {
       name: "Chinese",
-      shortName: "zh_CN",
+      shortName: "zh-CN",
       shortMonthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月",
                         "8月", "9月", "10月", "11月", "12月"],
 
